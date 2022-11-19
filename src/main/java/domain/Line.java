6a20fead739ac114d88cae7c03aabd73c80aaf0d
JanghoPark <pjhsk113@naver.com>
2022-11-19 18:27:02 +0900

    feat: 하나의 사다리 라인을 구성하는 일급컬렉션 생성

---
+++ src/main/java/domain/Line.java
@@ -0,0 +1,41 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Line {
+    private List<Point> lines;
+
+    private Line(List<Point> lines) {
+        this.lines = lines;
+    }
+
+    public static Line of(int countOfPerson, LineCreateStrategy strategy) {
+        return new Line(toPoint(countOfPerson, strategy));
+    }
+
+    private static List<Point> toPoint(int countOfPerson, LineCreateStrategy strategy) {
+        return createLine(countOfPerson, strategy)
+                .stream()
+                .map(Point::from)
+                .collect(Collectors.toList());
+    }
+
+    private static List<Boolean> createLine(int countOfPerson, LineCreateStrategy strategy) {
+        List<Boolean> points = new ArrayList<>();
+        boolean prev = false;
+        points.add(prev);
+        for (int i = 1; i < countOfPerson; i++) {
+            prev = strategy.createLine(prev);
+            points.add(prev);
+        }
+
+        return points;
+    }
+
+    public Stream<Point> stream() {
+        return lines.stream();
+    }
+}
