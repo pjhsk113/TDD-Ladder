7f7aa7d703c0f7013745a0faa50cf5f7aa52e818
JanghoPark <pjhsk113@naver.com>
2022-11-19 18:32:39 +0900

    feat: 입력받은 높이에 따라 N개의 Line을 가지는 사다리 객체 생성

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,5 +1,28 @@
 package domain;
 
-public class Ladder {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+public class Ladder {
+    private List<Line> ladder;
+
+    private Ladder(List<Line> ladder) {
+        this.ladder = ladder;
+    }
+
+    public static Ladder of(Participants participants, int height, LineCreateStrategy strategy) {
+        return new Ladder(toLine(participants.getSize(), height, strategy));
+    }
+
+    private static List<Line> toLine(int countOfPerson, int height, LineCreateStrategy strategy) {
+        return IntStream.range(0, height)
+                .mapToObj(value -> Line.of(countOfPerson, strategy))
+                .collect(Collectors.toList());
+    }
+
+    public Stream<Line> stream() {
+        return ladder.stream();
+    }
 }
