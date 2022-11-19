f0884377612621bbe25d0f491306d92f2c710f4a
JanghoPark <pjhsk113@naver.com>
2022-11-19 18:25:47 +0900

    feat: 사다리의 분기점을 만들어주는 사다리 랜덤 생성 전략 생성

---
+++ src/main/java/domain/LadderLineCreateStrategy.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.Random;
+
+public class LadderLineCreateStrategy implements LineCreateStrategy {
+    private static final Random random = new Random();
+
+    @Override
+    public boolean createLine(boolean prev) {
+        if (prev) {
+            return false;
+        }
+
+        return random.nextBoolean();
+    }
+}
---
+++ src/main/java/domain/LineCreateStrategy.java
@@ -0,0 +1,5 @@
+package domain;
+
+public interface LineCreateStrategy {
+    boolean createLine(boolean prev);
+}
