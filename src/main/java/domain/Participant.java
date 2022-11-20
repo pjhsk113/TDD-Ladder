41d9da9ac76ce77c15f1ef0ee2343297285fe827
JanghoPark <pjhsk113@naver.com>
2022-11-20 14:30:35 +0900

    test: 참가자 테스트
    - 이름에 빈 값이 입력된 경우 예외 발생 로직 추가

--- src/main/java/domain/Participant.java
+++ src/main/java/domain/Participant.java
@@ -2,6 +2,7 @@ package domain;
 
 public class Participant {
     private static final String OUT_OF_RANGE_NAME_LENGTH = "이름은 5글자를 초과할 수 없습니다.";
+    private static final String NAME_IS_EMPTY = "이름에 빈 값이 입력되었습니다.";
 
     private String name;
 
@@ -10,13 +11,20 @@ public class Participant {
     }
 
     public static Participant from(String name) {
-        validateName(name);
+        validateNameLength(name);
+        validateEmptyName(name);
         return new Participant(name);
     }
 
-    private static void validateName(String name) {
+    private static void validateNameLength(String name) {
         if (name.length() >  5) {
             throw new IllegalArgumentException(OUT_OF_RANGE_NAME_LENGTH);
+        }
+    }
+
+    private static void validateEmptyName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException(NAME_IS_EMPTY);
         }
     }
 
