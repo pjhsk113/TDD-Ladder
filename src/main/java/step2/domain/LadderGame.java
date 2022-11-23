f8f345576eec66a9cc3a0f1ce85db1970d60bfc6
JanghoPark <pjhsk113@naver.com>
2022-11-23 23:43:13 +0900

    refactor: 사다리 결과에 입력한 실행 결과가 함께 표시되도록 변경

--- src/main/java/step2/domain/LadderGame.java
+++ src/main/java/step2/domain/LadderGame.java
@@ -6,10 +6,10 @@ import step2.view.OutputView;
 public class LadderGame {
     public void play() {
         Participants participants = InputView.inputParticipant();
-        String expectResult = InputView.inputResult();
+        Results expectResult = InputView.inputResult(participants.getSize());
         int height = InputView.inputHeight();
 
         Ladder ladder = Ladder.of(participants, height, new LadderLineCreateStrategy());
-        OutputView.printLadder(participants, ladder);
+        OutputView.printLadder(participants, ladder, expectResult);
     }
 }
