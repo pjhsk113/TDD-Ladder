3ff3651c518162a75192437307afb8658862a5bd
JanghoPark <pjhsk113@naver.com>
2022-11-23 23:40:10 +0900

    feat: N개의 실행 결과를 담는 일급 컬랙션 생성

---
+++ src/main/java/step2/domain/Results.java
@@ -0,0 +1,45 @@
+package step2.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+
+public class Results {
+    private static final String COMMA = ",";
+
+    private List<Result> results;
+
+    private Results(List<Result> results) {
+        this.results = results;
+    }
+
+    public static Results from(String values, int limit) {
+        List<Result> results = convert(values);
+        validate(results, limit);
+        return new Results(results);
+    }
+
+    private static List<Result> convert(String values) {
+        return Arrays.stream(values.split(COMMA))
+                .map(Result::from)
+                .collect(Collectors.toList());
+    }
+
+    private static void validate(List<Result> results, int limit) {
+        if (results.size() > limit) {
+            throw new IllegalArgumentException("결과값은 참여자보다 많을 수 없습니다.");
+        }
+    }
+    public String getResults() {
+        return results.stream()
+                .map(result -> String.format("%6s", result.toString()))
+                .collect(joining(""));
+    }
+
+    public Stream<Result> stream() {
+        return results.stream();
+    }
+}
