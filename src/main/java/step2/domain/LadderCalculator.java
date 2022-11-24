fc04e7b30fb7986ca7d67713e878bcdeb6dde113
JanghoPark <pjhsk113@naver.com>
2022-11-24 19:46:26 +0900

    feat: 사다리 로직 계산 객체

---
+++ src/main/java/step2/domain/LadderCalculator.java
@@ -0,0 +1,9 @@
+package step2.domain;
+
+public class LadderCalculator {
+    public static Result execute(Ladder ladder, Participants participants, Results results) {
+        int index = 0;
+
+        // TODO 사다리 계산 로직 작성.... 어렵
+    }
+}
