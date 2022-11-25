6cb4025aa9157f6d1d3bcd5a1177c27108e030d7
JanghoPark <pjhsk113@naver.com>
2022-11-25 21:24:43 +0900

    refactor: 참가자가 참가번호(index)를 가지도록 변경

--- src/main/java/step2/domain/Participant.java
+++ src/main/java/step2/domain/Participant.java
@@ -4,16 +4,18 @@ public class Participant {
     private static final String OUT_OF_RANGE_NAME_LENGTH = "이름은 5글자를 초과할 수 없습니다.";
     private static final String NAME_IS_EMPTY = "이름에 빈 값이 입력되었습니다.";
 
+    private int participationNumber;
     private String name;
 
-    private Participant(String name) {
+    private Participant(int participationNumber, String name) {
+        this.participationNumber = participationNumber;
         this.name = name;
     }
 
-    public static Participant from(String name) {
+    public static Participant from(int participationNumber, String name) {
         validateNameLength(name);
         validateEmptyName(name);
-        return new Participant(name);
+        return new Participant(participationNumber, name);
     }
 
     private static void validateNameLength(String name) {
@@ -26,6 +28,10 @@ public class Participant {
         if (name.trim().isEmpty()) {
             throw new IllegalArgumentException(NAME_IS_EMPTY);
         }
+    }
+
+    public boolean isTarget(String target) {
+        return name.equals(target);
     }
 
     @Override
