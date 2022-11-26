b0e7e69e41a303d6e3264352d075b0c593ceb2ed
JanghoPark <pjhsk113@naver.com>
2022-11-26 17:14:54 +0900

    refactor: "all"을 입력받았을 경우 전체 결과 출력하도록 분기처리

--- src/main/java/step2/domain/LadderCalculator.java
+++ src/main/java/step2/domain/LadderCalculator.java
@@ -1,10 +1,18 @@
 package step2.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LadderCalculator {
     private LadderCalculator() { }
 
-    public static Result execute(Ladder ladder, Participant participant, Results results) {
-        int resultIndex = ladder.execute(participant);
-        return results.findResult(resultIndex);
+    public static List<Integer> execute(Ladder ladder, Participants participants, String target) {
+        if (target.equalsIgnoreCase("all")) {
+            return ladder.executeAll(participants);
+        }
+
+        return Arrays.asList(
+                ladder.execute(participants.findParticipant(target))
+        );
     }
 }
