826345d9d62c405c649f4b6ebf61df5a771b28c0
JanghoPark <pjhsk113@naver.com>
2022-11-26 15:15:36 +0900

    feat: Point가 index를 가지고 자신의 방향을 계산하는 로직 추가

--- src/main/java/step2/domain/Point.java
+++ src/main/java/step2/domain/Point.java
@@ -1,17 +1,51 @@
 package step2.domain;
 
 public class Point {
+    private int index;
     private boolean divergingPoint;
 
-    private Point(boolean divergingPoint) {
+    private Point(int index, boolean divergingPoint) {
+        this.index = index;
         this.divergingPoint = divergingPoint;
     }
 
-    public static Point from(boolean divergingPoint) {
-        return new Point(divergingPoint);
+    public static Point of(int index, boolean divergingPoint) {
+        return new Point(index, divergingPoint);
     }
 
     public boolean isDivergingPoint() {
         return divergingPoint;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int isRightDiverge(Point point) {
+        if (point.isDivergingPoint()) {
+            return point.getIndex();
+        }
+
+        return index;
+    }
+
+    public int isLeftDiverge() {
+        if (divergingPoint) {
+            return index - 1;
+        }
+
+        return index;
+    }
+
+    public int findDirectionIndex(Point next) {
+        if (divergingPoint) {
+            return index - 1;
+        }
+
+        if (next.isDivergingPoint()) {
+            return index + 1;
+        }
+
+        return index;
     }
 }
