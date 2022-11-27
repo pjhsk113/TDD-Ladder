ba3deefe721fe9589cb9fad06b970c71c56a2e73
JanghoPark <pjhsk113@naver.com>
2022-11-27 14:53:49 +0900

    test: Results 테스트 추가
    - Nested 사용시 methodSource는 상위 클래스에 정의되어 있어야함.
    또한 해당 패키지 경로를 명시해줘야 올바르게 동작함

---
+++ src/test/java/step2/domain/ResultsTest.java
@@ -0,0 +1,72 @@
+package step2.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultsTest {
+    @DisplayName("참가자 수 보다 결괏값이 더 많이 입력된 경우 예외 발생")
+    @ParameterizedTest
+    @CsvSource(value = {"당첨, 꽝, 꽝, 당첨 : 3", "당첨, 꽝, 꽝 : 2"}, delimiter = ':')
+    void inputExceededExceptionTest(String values, int limit) {
+        assertThatThrownBy(() -> Results.of(values, limit))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력된 결과를 올바른 포맷으로 반환하는지 테스트")
+    @ParameterizedTest
+    @MethodSource("resultsProvider")
+    void resultsFormattingTest(Results results, String expected) {
+        assertThat(results.getResults()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> resultsProvider() {
+        return Stream.of(
+                Arguments.of(Results.of("0,0,1000,0", 4), "     0     0  1000     0"),
+                Arguments.of(Results.of("꽝,당첨,꽝,꽝", 4), "     꽝    당첨     꽝     꽝")
+        );
+    }
+
+    @Nested
+    @DisplayName("결과값 반환 테스트")
+    class ReturnResultsTest {
+        @DisplayName("올바른 매칭된 결과값을 반환")
+        @ParameterizedTest
+        @MethodSource("step2.domain.ResultsTest#matchingResultsProvider")
+        void resultsMatchingTest(Results results, int resultIndex, Result expected) {
+            assertThat(results.findResult(resultIndex).toString()).isEqualTo(expected.toString());
+        }
+
+        @DisplayName("매칭된 결과값이 없다면 예외 발생")
+        @ParameterizedTest
+        @MethodSource("step2.domain.ResultsTest#notMatchingResultsProvider")
+        void resultsNotMatchingTest(Results results, int resultIndex) {
+            assertThatThrownBy(() -> results.findResult(resultIndex))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    private static Stream<Arguments> matchingResultsProvider() {
+        return Stream.of(
+                Arguments.of(Results.of("0,0,1000,0", 4), 2, Result.of(2, "1000")),
+                Arguments.of(Results.of("꽝,당첨,꽝,꽝", 4), 1, Result.of(1, "당첨"))
+        );
+    }
+
+    private static Stream<Arguments> notMatchingResultsProvider() {
+        return Stream.of(
+                Arguments.of(Results.of("0,0,1000,0", 4), 5),
+                Arguments.of(Results.of("꽝,당첨,꽝,꽝", 4), 5)
+        );
+    }
+}
